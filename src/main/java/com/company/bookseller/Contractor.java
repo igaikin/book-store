b9f376767e42cb7853b5d6367880d658d298baec
Gaikin <gaikin@icloud.com>
2021-12-30 12:09:34 +0300

    minor edit Contractor structure

--- src/main/java/com/company/bookseller/Contractor.java
+++ src/main/java/com/company/bookseller/Contractor.java
@@ -1,15 +1,15 @@
 package com.company.bookseller;
 
 import com.company.bookseller.model.beans.Book;
-//import com.company.bookseller.model.beans.Order;
-//import com.company.bookseller.model.beans.User;
+import com.company.bookseller.model.beans.Order;
+import com.company.bookseller.model.beans.User;
 import com.company.bookseller.model.dao.connection.ConnectionManager;
 import com.company.bookseller.model.service.BookService;
-//import com.company.bookseller.model.service.OrderService;
-//import com.company.bookseller.model.service.UserService;
+import com.company.bookseller.model.service.OrderService;
+import com.company.bookseller.model.service.UserService;
 import com.company.bookseller.model.service.impl.BookServiceImpl;
-//import com.company.bookseller.model.service.impl.OrderServiceImpl;
-//import com.company.bookseller.model.service.impl.UserServiceImpl;
+import com.company.bookseller.model.service.impl.OrderServiceImpl;
+import com.company.bookseller.model.service.impl.UserServiceImpl;
 import com.company.bookseller.model.service.util.Printer;
 import org.apache.logging.log4j.Logger;
 
@@ -20,48 +20,60 @@ public class Contractor {
     public static void main(String[] args) {
 
         BookService bookService = new BookServiceImpl();
-//        UserService userService = new UserServiceImpl();
-//        OrderService orderService = new OrderServiceImpl();
-
-//
         List<Book> previewBooks = bookService.getAll();
-        String table = Printer.getBookFormattedTable(previewBooks);
-        System.out.println(table);
+        String tableBooks = Printer.getBookFormattedTable(previewBooks);
+        System.out.println(tableBooks);
 
-//        List<User> previewUsers = userService.getAll();
-//        String table = Printer.getUserFormattedTable(previewUsers);
-//        System.out.println(table);
+        UserService userService = new UserServiceImpl();
+        List<User> previewUsers = userService.getAll();
+        String tableUsers = Printer.getUserFormattedTable(previewUsers);
+        System.out.println(tableUsers);
 
-//        List<Order> orders = orderService.getAll();
-//        orders.forEach(System.out::println);
+        OrderService orderService = new OrderServiceImpl();
+        List<Order> prewieOrders = orderService.getAll();
+        String tableOrders = Printer.getOrderFormattedTable(prewieOrders);
+        System.out.println(tableOrders);
 
         Scanner scanner = new Scanner(System.in);
         try {
             while (true) {
-                System.out.print("Please enter the ID to see detailed information for the list item: ");
+                System.out.print("Please enter the ID to see detailed information for the list Books: ");
                 long id = scanner.nextLong();
                 if (id <= 0) {
                     break;
                 }
-
                 Book book = bookService.getById(id);
                 if (book != null) {
                     System.out.println(book.getFormattedOutput());
                 } else {
                     System.out.println("Book with this ID does not exist");
                 }
-//                User user = userService.getById(id);
-//                if (user != null) {
-//                    System.out.println(user.getFormattedOutput());
-//                } else {
-//                    System.out.println("User with this ID does not exist");
-//                }
-//                Order order = orderService.getById(id);
-//                if (order != null) {
-//                    System.out.println(order.getFormattedOutput());
-//                } else {
-//                    System.out.println("Order with this ID does not exist");
-//                }
+            }
+            while (true) {
+                System.out.print("Please enter the ID to see detailed information for the list Users: ");
+                long id = scanner.nextLong();
+                if (id <= 0) {
+                    break;
+                }
+                User user = userService.getById(id);
+                if (user != null) {
+                    System.out.println(user.getFormattedOutput());
+                } else {
+                    System.out.println("User with this ID does not exist");
+                }
+            }
+            while (true) {
+                System.out.print("Please enter the ID to see detailed information for the list Orders: ");
+                long id = scanner.nextLong();
+                if (id <= 0) {
+                    break;
+                }
+                Order order = orderService.getById(id);
+                if (order != null) {
+                    System.out.println(order.getFormattedOutput());
+                } else {
+                    System.out.println("Order with this ID does not exist");
+                }
             }
         } finally {
             ConnectionManager.getInstance().tearDown();
