ad8c0debc7dfd855c9e20876310790d556f821d9
Gaikin <gaikin@icloud.com>
2022-06-17 23:39:15 +0300

    refactoring AddToCardCommand

--- src/main/java/com/company/bookseller/controller/commands/impl/AddToCartCommand.java
+++ src/main/java/com/company/bookseller/controller/commands/impl/AddToCartCommand.java
@@ -2,30 +2,46 @@ package com.company.bookseller.controller.commands.impl;
 
 import com.company.bookseller.controller.commands.Command;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.Map;
 
 public class AddToCartCommand implements Command {
     @Override
-    @SuppressWarnings("unchecked")
     public String execute(HttpServletRequest req) {
+        HttpSession session = req.getSession();
+        Map<Long, Integer> cart = getCart(session);
+
         Long bookId = Long.valueOf(req.getParameter("book_id"));
-        Object rawCart = req.getSession().getAttribute("cart");
+        addBookToCart(bookId, cart);
+        session.setAttribute("cart", cart);
+
+        return getReturnPage(req, "jsp/allBooks.jsp");
+    }
+
+    private String getReturnPage(HttpServletRequest req, String defaultValue) {
+        String from = req.getParameter("from");
+        return from != null ? from : defaultValue;
+    }
+
+    private void addBookToCart(Long bookId, Map<Long, Integer> cart) {
+        Integer quantity = cart.get(bookId);
+        if (quantity == null) {
+            cart.put(bookId, 1);
+        } else {
+            cart.put(bookId, quantity + 1);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private Map<Long, Integer> getCart(HttpSession session) {
+        Object rawCart = session.getAttribute("cart");
         Map<Long, Integer> cart;
         if (rawCart != null) {
             cart = (Map<Long, Integer>) rawCart;
         } else {
             cart = new HashMap<>();
         }
-        Integer quantity = cart.get(bookId);
-        if (quantity == null) {
-            cart.put(bookId, 1);
-        } else {
-            cart.put(bookId, ++quantity);
-        }
-        req.getSession().setAttribute("cart", cart);
-        String from = req.getParameter("from");
-
-        return from != null ? from : "jsp/allBooks.jsp";
+        return cart;
     }
 }
