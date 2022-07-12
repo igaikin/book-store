c28ae6018ee0c77d449b39680b0b792be0424d87
Gaikin <gaikin@icloud.com>
2022-07-12 17:51:36 +0300

    attempt to implement language change

--- src/main/java/com/company/bookseller/controller/commands/impl/ChangeLanguageCommand.java
+++ src/main/java/com/company/bookseller/controller/commands/impl/ChangeLanguageCommand.java
@@ -2,10 +2,37 @@ package com.company.bookseller.controller.commands.impl;
 
 import com.company.bookseller.controller.commands.Command;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 
 public class ChangeLanguageCommand implements Command {
     @Override
     public String execute(HttpServletRequest req) {
-        return null;
+        HttpSession session = req.getSession();
+        session.getAttribute(String.valueOf(Locale.getDefault()));
+        //FIXME
+
+
+        return req.getParameter("");
     }
+
+    private Map<String, Locale> lang;
+
+    private void Language() {
+        lang = new HashMap<>();
+        lang.put("EN", Locale.ENGLISH);
+        lang.put("DE", Locale.GERMAN);
+        lang.put("RU", new Locale("ru", "RU"));
+    }
+
+    public Locale getLanguage(String language) {
+        if (language != null) {
+            return lang.get(language);
+        }
+        return lang.getOrDefault("EN", Locale.ENGLISH);
+    }
+
+
 }
