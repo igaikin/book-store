219b38e0a133391d33eb15da3364d3b0132ed7d2
Gaikin <gaikin@icloud.com>
2022-07-26 22:25:02 +0300

    fix change language command

--- src/main/java/com/company/bookseller/controller/commands/impl/ChangeLanguageCommand.java
+++ src/main/java/com/company/bookseller/controller/commands/impl/ChangeLanguageCommand.java
@@ -1,6 +1,7 @@
 package com.company.bookseller.controller.commands.impl;
 
 import com.company.bookseller.controller.commands.Command;
+import com.company.bookseller.util.MessageManager;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import java.util.HashMap;
@@ -10,29 +11,28 @@ import java.util.Map;
 public class ChangeLanguageCommand implements Command {
     @Override
     public String execute(HttpServletRequest req) {
+        String lang = req.getParameter( "lang");
         HttpSession session = req.getSession();
-        session.getAttribute(String.valueOf(Locale.getDefault()));
-        //FIXME
-
-
-        return req.getParameter("");
+        session.setAttribute("language", lang);
+        MessageManager.setLocale(lang);
+        return "index.jsp";
     }
 
-    private Map<String, Locale> lang;
-
-    private void Language() {
-        lang = new HashMap<>();
-        lang.put("EN", Locale.ENGLISH);
-        lang.put("DE", Locale.GERMAN);
-        lang.put("RU", new Locale("ru", "RU"));
-    }
-
-    public Locale getLanguage(String language) {
-        if (language != null) {
-            return lang.get(language);
-        }
-        return lang.getOrDefault("EN", Locale.ENGLISH);
-    }
-
+//    private Map<String, Locale> lang;
+//
+//    private void Language() {
+//        lang = new HashMap<>();
+//        lang.put("EN", Locale.ENGLISH);
+//        lang.put("DE", Locale.GERMAN);
+//        lang.put("RU", new Locale("ru", "RU"));
+//    }
+//
+//    public Locale getLanguage(String language) {
+//        if (language != null) {
+//            return lang.get(language);
+//        }
+//        return lang.getOrDefault("EN", Locale.ENGLISH);
+//    }
+//
 
 }
