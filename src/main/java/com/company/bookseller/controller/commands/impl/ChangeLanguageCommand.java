10e9c5ce2eacfd431ede59a6a2cc3f8cb58f7281
Gaikin <gaikin@icloud.com>
2022-08-02 01:03:32 +0300

    formatting fix

--- src/main/java/com/company/bookseller/controller/commands/impl/ChangeLanguageCommand.java
+++ src/main/java/com/company/bookseller/controller/commands/impl/ChangeLanguageCommand.java
@@ -4,35 +4,14 @@ import com.company.bookseller.controller.commands.Command;
 import com.company.bookseller.util.MessageManager;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
 
 public class ChangeLanguageCommand implements Command {
     @Override
     public String execute(HttpServletRequest req) {
-        String lang = req.getParameter( "lang");
+        String lang = req.getParameter("lang");
         HttpSession session = req.getSession();
         session.setAttribute("language", lang);
         MessageManager.setLocale(lang);
         return "index.jsp";
     }
-
-//    private Map<String, Locale> lang;
-//
-//    private void Language() {
-//        lang = new HashMap<>();
-//        lang.put("EN", Locale.ENGLISH);
-//        lang.put("DE", Locale.GERMAN);
-//        lang.put("RU", new Locale("ru", "RU"));
-//    }
-//
-//    public Locale getLanguage(String language) {
-//        if (language != null) {
-//            return lang.get(language);
-//        }
-//        return lang.getOrDefault("EN", Locale.ENGLISH);
-//    }
-//
-
 }
